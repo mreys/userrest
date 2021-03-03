9b81dc80c8f76f68e410d2fcf3c86853d6ac5405
mreys <mike.reys@gmail.com>
2021-03-03 15:28:20 +0000

    Adding the framework

---
+++ src/main/java/com/example/userrest/UserrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.userrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userrest/UserrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
